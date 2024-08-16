84664064a573249328048a5e2dfed29b2f338a37
Md. Sibgatur Rahman Efti <sibgaturrahmanbros@gmail.com>
2024-08-16 11:13:54 +0600

    first commit

---
+++ box_color_with_DOM/script.js
@@ -0,0 +1,14 @@
+// let boxes = document.getElementsByClassName("box")
+let boxes = document.querySelector(".container").children
+
+function getRandomColor(){
+    let v1 = Math.ceil(Math.random()*255)
+    let v2 = Math.ceil(Math.random()*255)
+    let v3 = Math.ceil(Math.random()*255)
+    return `rgb(${v1},${v2},${v3})`
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.color = getRandomColor()
+    e.style.backgroundColor = getRandomColor()
+})
