b6a9bc1706eb1b6015f0cba993545d68b377c87f
Md. Sibgatur Rahman Efti <sibgaturrahmanbros@gmail.com>
2024-08-19 00:08:25 +0600

    some minor changes in if else condition and string

--- dynamic_website_builder/script.js
+++ dynamic_website_builder/script.js
@@ -3,16 +3,19 @@ function createCard(title, cName, views, monthsOld, duration, thumbnail){
     document.querySelector(".duration").innerText = duration
     document.querySelector(".title").firstElementChild.innerHTML = title
     document.querySelector(".cName").innerText = cName
-    if(999 < views && views < 1000001){
+    if(views <1000){
+        document.querySelector(".number").innerText = views
+    }
+    else if(views < 1000001){
         let num = views/1000
-        document.querySelector(".number").innerText = String(num.toPrecision(3)) + "K"
+        document.querySelector(".number").innerText = num.toPrecision(3) + "K"
 
     }
-    else if(1000001 < views && views < 1000000001){
+    else if(1000000 <= views){
         let num = views/1000000
-        document.querySelector(".number").innerText = String(num.toPrecision(3).toPrecesion(2)) + "M"
+        document.querySelector(".number").innerText = num.toPrecesion(2) + "M"
     }
     document.querySelector(".monthsOld").firstElementChild.innerText = monthsOld
 }
 
-createCard("JavaScript Exercise 13 - Dynamic Website Builder | Sigma Web Development Course - Tutorial #73", "CodeWithHarry", "73894", "8", "6:13" ,"https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLABJhdIBqlilwXKzJqa6StynbJT4Q")
+createCard("JavaScript Exercise 13 - Dynamic Website Builder | Sigma Web Development Course - Tutorial #73", "CodeWithHarry", "73", "8", "6:13" ,"https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLABJhdIBqlilwXKzJqa6StynbJT4Q")
